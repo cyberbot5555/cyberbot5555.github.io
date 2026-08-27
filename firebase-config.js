7fbdfff7a24476bad3a0d2aba9ee2f3d3eaa169d
Md Solayman Hossain Shawon <shawon242-15-555@diu.edu.bd>
2026-08-28 00:21:28 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQMFTupW88Ynzeq_QaVcoKH1TM8vCIls8",
+  authDomain: "cyberbot-portfolio.firebaseapp.com",
+  projectId: "cyberbot-portfolio",
+  storageBucket: "cyberbot-portfolio.firebasestorage.app",
+  messagingSenderId: "581947769621",
+  appId: "1:581947769621:web:9edf98046f943032c71f1c",
+  measurementId: "G-2KV9CDDGGQ"
+};
